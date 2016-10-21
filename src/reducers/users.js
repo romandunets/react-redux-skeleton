c3c7f29608518210db3ebb109fe24759423b4e47
Roman Dunets <roman.dunets.email@gmail.com>
2016-10-21 20:02:44 +0300

    Add users reducer

---
+++ src/reducers/users.js
@@ -0,0 +1,15 @@
+const user = (state = [], action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+const users = (state = [], action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default users;
