da2d1e3839d7a631a95ef1a6a0c9fbd93e9f4742
Roman Dunets <roman.dunets.email@gmail.com>
2016-10-27 23:46:50 +0300

    Add actions for asynchronous fetching of users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,27 @@
+import axios from 'axios';
+
 export function fetchUsers() {
+  return function(dispatch) {
+    axios.get('http://0.0.0.0:9000/users')
+      .then(function (response) {
+        dispatch(fetchUsersSuccess(response));
+      })
+      .catch(function (error) {
+        console.log(fetchUsersFail(error));
+      });
+  }
+}
+
+function fetchUsersSuccess(users) {
   return {
-    type: "FETCH_USERS",
-    payload: [
-      { id: 1, username: "testuser1" },
-      { id: 2, username: "testuser2" },
-      { id: 3, username: "testuser3" }
-    ]
+    type: "FETCH_USERS_SUCCESS",
+    payload: { users }
+  }
+}
+
+function fetchUsersFail(error) {
+  return {
+    type: "FETCH_USERS_FAIL",
+    payload: { error }
   }
 }
